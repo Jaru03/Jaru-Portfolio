7291e1094e014b5a329840a8a0ed096902681634
Jaru03 <155789206+Jaru03@users.noreply.github.com>
2024-04-18 15:47:40 -0400

    Portfolio

--- src/components/main/ButtonFixed.tsx
+++ src/components/main/ButtonFixed.tsx
@@ -3,8 +3,8 @@ import './style/ButtonFixed.css'
 const ButtonFixed = () => {
   return (
     <aside className="buttonsFixed">
-      <article className="buttonFix button__email"><a href="" className="buttonFix__link"><img className='button__img' src="../.../../public/email.png" alt="" /></a></article>
-      <article className="buttonFix button__whatsapp"><a href="" className="buttonFix__link"><img className='button__img' src="../.../../public/whatsapp.png" alt="" /></a></article>
+      <article className="buttonFix button__email"><a href="" className="buttonFix__link"><img className='button__img' src="../.../../email.png" alt="" /></a></article>
+      <article className="buttonFix button__whatsapp"><a href="" className="buttonFix__link"><img className='button__img' src="../.../../whatsapp.png" alt="" /></a></article>
     </aside>
   )
 }
--- src/components/main/Skills.tsx
+++ src/components/main/Skills.tsx
@@ -5,12 +5,12 @@ const Skills = () => {
     <section className="skills">
       <h2 className="skills__tittle subtittle">Habilidades</h2>
       <div className="skills__container">
-        <article className='skills__item'> <img className='skills__img' src="../../../../public/html.png" alt="" /></article>
-        <article className='skills__item'><img className='skills__img' src="../../../../public/CSS.png" alt="" /></article>
-        <article className='skills__item'><img className='skills__img' src="../../../../public/js.png" alt="" /></article>
-        <article className='skills__item'><img className='skills__img' src="../../../../public/React.png" alt="" /></article>
-        <article className='skills__item'><img className='skills__img' src="../../../../public/tailwind.png" alt="" /></article>
-        <article className='skills__item'><img className='skills__img' src="../../../../public/TypeScript.png" alt="" /></article>
+        <article className='skills__item'> <img className='skills__img' src="../../../../html.png" alt="" /></article>
+        <article className='skills__item'><img className='skills__img' src="../../../../CSS.png" alt="" /></article>
+        <article className='skills__item'><img className='skills__img' src="../../../../js.png" alt="" /></article>
+        <article className='skills__item'><img className='skills__img' src="../../../../React.png" alt="" /></article>
+        <article className='skills__item'><img className='skills__img' src="../../../../tailwind.png" alt="" /></article>
+        <article className='skills__item'><img className='skills__img' src="../../../../TypeScript.png" alt="" /></article>
       </div>
     </section>
   )
