bc3301b4c2f4e56607d1f85fbf18f3a60e321ae9
Jaru03 <155789206+Jaru03@users.noreply.github.com>
2024-04-18 15:50:22 -0400

    Portfolio

--- src/components/main/Skills.tsx
+++ src/components/main/Skills.tsx
@@ -6,7 +6,7 @@ const Skills = () => {
       <h2 className="skills__tittle subtittle">Habilidades</h2>
       <div className="skills__container">
         <article className='skills__item'> <img className='skills__img' src="../../../../html.png" alt="" /></article>
-        <article className='skills__item'><img className='skills__img' src="../../../../CSS.png" alt="" /></article>
+        <article className='skills__item'><img className='skills__img' src="../../../../css.png" alt="" /></article>
         <article className='skills__item'><img className='skills__img' src="../../../../js.png" alt="" /></article>
         <article className='skills__item'><img className='skills__img' src="../../../../React.png" alt="" /></article>
         <article className='skills__item'><img className='skills__img' src="../../../../tailwind.png" alt="" /></article>
